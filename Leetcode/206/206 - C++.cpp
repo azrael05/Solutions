a33edf6b562fec315851fba767644741c26fea68
azrael18102001 <105088428+azrael18102001@users.noreply.github.com>
2022-08-18 12:45:22 +0530

    updated

--- Leetcode/206/206 - C++.cpp
+++ Leetcode/206/206 - C++.cpp
@@ -1,10 +1,14 @@
-// Problem Number - 206. Reverse a Linked List
-// Problem URL - https://leetcode.com/problems/reverse-linked-list/
+/*  Problem Number - 206. Reverse a Linked List
+    Problem URL - https://leetcode.com/problems/reverse-linked-list/
 
-/* Submitted by Devesh Kumar 
-LinkedIn- linkedin.com/in/devesh-k-110862113/
+    Submitted by Devesh Kumar 
+	LinkedIn- linkedin.com/in/devesh-k-110862113/
+	Time complexity - O(n)
+	Space complexity - O(1)
 
-/**
+
+
+
  * Definition for singly-linked list.
  * struct ListNode {
  *     int val;
@@ -17,20 +21,21 @@ LinkedIn- linkedin.com/in/devesh-k-110862113/
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
+    	/* If linked list is empty return head pointer*/
         if(head==NULL)
         {
             return head;
         }
-        ListNode *b=head,*f=head->next,*temp;
-        head->next=NULL;
-        for(;f!=NULL;)
+        ListNode *back=head,*front=head->next,*temp;
+        head->next=NULL;  //Set the next of head node as NULL
+        for(;front!=NULL;)
         {
-            temp=f->next;
-            f->next=b;
-            b=f;
-            f=temp;
+            temp=front->next;     //store the next node in temporary pointer
+            front->next=back;		  // set the value of next in current node to point to previous node 
+            back=front;			  // make current node to previous node for next iteration
+            front=temp;			  // shift next node to current node for next iteration
         }
             
-        return b;
+        return back;       //return back which would be pointing at the orignally last Node
     }
 };
