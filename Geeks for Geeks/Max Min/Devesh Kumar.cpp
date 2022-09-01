cb36b5db334ab422372c3025027159a18320e20b
azrael18102001 <105088428+azrael18102001@users.noreply.github.com>
2022-09-02 01:50:24 +0530

    devesh

---
+++ Geeks for Geeks/Max Min/Devesh Kumar.cpp
@@ -0,0 +1,63 @@
+/*
+	Problem Link - https://practice.geeksforgeeks.org/problems/reverse-a-string
+	
+	Submitted By - Devesh Kumar
+	
+	Time complexity - O(n)
+	Space Complexity - O(1)
+*/
+
+
+
+//{ Driver Code Starts
+/* Driver program to test above function */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+   public:
+    int findSum(int A[], int N)
+    {
+    	int min=INT_MAX,max=INT_MIN;
+    	for(int i=0;i<N;i++)
+    	{
+    	    if(A[i]>max)
+    	    {
+    	        max=A[i];
+    	    }
+    	    if(A[i]<min)
+    	    {
+    	        min=A[i];
+    	    }
+    	}
+    	return min+max;
+    }
+
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++)
+	      cin>>arr[i];
+	    Solution ob;  
+	    int ans=ob.findSum(arr, n);
+	    cout<<ans;
+	    cout<<"\n";
+	}
+	return 0;
+}
+
+// } Driver Code Ends
